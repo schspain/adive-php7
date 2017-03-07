ef8c4f548ff35818cb6a9a071b5edd42f860443e
Adive <ferdinandmartin@users.noreply.github.com>
2017-03-07 10:29:57 +0100

    Update hello.php

--- Views/Default/hello.php
+++ Views/Default/hello.php
@@ -1,1 +1,14 @@
-<?php
+    <h1>Hello <?=$example?>!</h1>
+    <p class="lead">This is a "Hello World!" example, passing a $var in the URL like <code>hello/John</code>.</p>
+    
+    <section style="padding-top: 20px">
+        <h3>Edit this "Hello World!" example</h3>
+        <p>
+            The Controller is located on <code>Controller/Default.php</code>, edit this file and change it, go ahead!
+        </p>
+
+        <h3>Twitter</h3>
+        <p>
+            Visit <a href="https://github.com/ferdinandmartin/pulse-php7" target="_blank">Pulse PHP7 GitHub</a> site to get in touch, watch it, star it and fork it!.
+        </p>
+    </section>
