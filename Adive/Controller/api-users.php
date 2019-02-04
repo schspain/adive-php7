f0e8387005ba9e799a0813333fe277ff0cf876df
Adive <ferdinandmartin@users.noreply.github.com>
2019-02-04 16:34:33 +0100

    Update api-users.php
    Important bug-fix: security improvement

--- Adive/Controller/api-users.php
+++ Adive/Controller/api-users.php
@@ -11,7 +11,7 @@
 // @Route(GET)
 $API->get('/admin/user', 
     function() use($API, $db) {
-        if(isset($_SESSION['adive.id'])){
+        if(!isset($_SESSION['adive.id'])){ $API->redirect($API->urlFor('alogin')); }
             pathActive('config');
             $tablesQuery = $db->prepare('SELECT * FROM `adive_users` WHERE permissions != 1 ORDER BY username');
             $tablesQuery->execute();
@@ -22,15 +22,13 @@ $API->get('/admin/user',
                 'description' => 'Users for public dashboard.',
                 'result' => $resUser
             ));
-        } else {
-            $API->redirect($API->urlFor('alogin'));
-        }
     }
 )->name('auser');
 
 // @Route(GET)
 $API->get('/admin/user/add', 
     function() use($API, $db) {
+        if(!isset($_SESSION['adive.id'])){ $API->redirect($API->urlFor('alogin')); }
             pathActive('config');
             
             $query = $db->prepare("SELECT * FROM adive_users ORDER by id ASC");
@@ -48,6 +46,7 @@ $API->get('/admin/user/add',
 // @Route(POST)
 $API->post('/admin/user/add', 
     function() use($API, $db) {
+        if(!isset($_SESSION['adive.id'])){ $API->redirect($API->urlFor('alogin')); }
         $formData=$API->request;
 
         if($formData->post('pass')!=''){
@@ -91,6 +90,7 @@ $API->post('/admin/user/add',
 // @Route(GET)
 $API->get('/admin/user/edit/{user_id}', 
     function($userid) use($API, $db) {
+        if(!isset($_SESSION['adive.id'])){ $API->redirect($API->urlFor('alogin')); }
         pathActive('config');
         $query1 = $db->prepare("SELECT * FROM adive_users WHERE id=:userid ORDER by id ASC");
         $query1->execute(array(
@@ -109,6 +109,7 @@ $API->get('/admin/user/edit/{user_id}',
 // @Route(POST)
 $API->post('/admin/user/edit/{user_id}', 
     function($userid) use($API, $db) {
+        if(!isset($_SESSION['adive.id'])){ $API->redirect($API->urlFor('alogin')); }
 
         $formData=$API->request;
         
@@ -148,6 +149,7 @@ $API->post('/admin/user/edit/{user_id}',
 // @Route(GET) DELETE table
 $API->get('/admin/user/delete/{nav_id}', 
     function($userid) use($API, $db) {
+        if(!isset($_SESSION['adive.id'])){ $API->redirect($API->urlFor('alogin')); }
         pathActive('config');
         
         $query2 = $db->prepare("DELETE FROM adive_users WHERE id=:userid");
